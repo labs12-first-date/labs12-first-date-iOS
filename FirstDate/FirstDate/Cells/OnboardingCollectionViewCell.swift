1e948a18122cb0fd99f5e981c4925c0402b42091
Frulwinn <frulwinn@gmail.com>
2019-05-03 10:15:56 -0500

    added onboarding and get information view controllers to storyboard

--- FirstDate/FirstDate/Cells/OnboardingCollectionViewCell.swift
+++ FirstDate/FirstDate/Cells/OnboardingCollectionViewCell.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
     
-    //MARK: - Properties
-    private var labels: [UILabel]!
+    @IBOutlet weak var textLabel: UILabel!
     
     
-    //MARK: - Outlets
-    @IBOutlet weak var onboardingLabel: UILabel!
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let textLabel = UILabel(frame: .zero)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            textLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            textLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            ])
+        self.textLabel = textLabel
+        
+        self.contentView.backgroundColor = .lightGray
+        self.textLabel.textAlignment = .center
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        fatalError("Interface Builder is not supported!")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        labels = [onboardingLabel]
+        
+        fatalError("Interface Builder is not supported!")
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.textLabel.text = nil
+    }
 }
