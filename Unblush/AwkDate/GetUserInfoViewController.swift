e7c488fd6773593c4ed7a5147d7d6bacdc2a7a3e
Frulwinn <frulwinn@gmail.com>
2019-05-08 11:21:07 -0500

    re added files since starting from new file

---
+++ Unblush/AwkDate/GetUserInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GetUserInfoViewController.swift
+//  AwkDate
+//
+//  Created by Lambda_School_Loaner_34 on 5/8/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class GetUserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
