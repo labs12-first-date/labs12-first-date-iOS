2d488781eb23bb404facca5b9214fde88902c522
Frulwinn <frulwinn@gmail.com>
2019-05-08 12:23:37 -0500

    completed onboarding view controller

--- Unblush/AwkDate/GetUserInfoViewController.swift
+++ Unblush/AwkDate/GetUserInfoViewController.swift
@@ -9,22 +9,80 @@
 import UIKit
 
 class GetUserInfoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //MARK: - Properties
+    var userController2: UserController2?
+    var profile: Profile?
+    private var datePicker: UIDatePicker?
+    
+    //MARK: - Outlets
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var zipLabel: UILabel!
+    @IBOutlet weak var zipTextField: UITextField!
+    
+    @IBOutlet weak var laterButton: UIButton!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBAction func saveButton(_ sender: Any) {
+        guard let firstName = firstNameTextField.text, !firstName.isEmpty,
+            let lastName = lastNameTextField.text,
+            let dateOfBirth = dateOfBirthTextField.text,
+            let zip = zipTextField.text else { return }
+        
+        //        let date = self.dateFormatter.date(from: dateOfBirth)!
+        
+        //        if let user = user {
+        //            userController?.update
+        //        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //date picker
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(GetUserInfoViewController.dateChanged(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(GetUserInfoViewController.viewTapped(gestureRecognizer:)))
+        
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+        
+        dateOfBirthTextField.inputView = datePicker
     }
-    */
-
+    
+    @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d"
+        
+        dateOfBirthTextField.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+        
+    }
+    // Do any additional setup after loading the view.        }
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        guard let profile = profile
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SaveGetInfo" {
+            guard segue.destination is BioConditionViewController else { return }
+        }
+    }
+    
 }
+
--- Unblush/AwkDate/OnboardingViewController.swift
+++ Unblush/AwkDate/OnboardingViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var onboardingLabel: UILabel!
+    @IBOutlet weak var getStartedButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +20,10 @@ class OnboardingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Getstarted" {
+        guard segue.destination is GetUserInfoViewController else { return }
+        }
     }
-    */
 
 }
