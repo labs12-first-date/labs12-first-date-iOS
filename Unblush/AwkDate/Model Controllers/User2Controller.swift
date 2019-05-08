df8b1a4b44eb3af12f4cff6dd0a47348a5fe5ba2
jocate <jocelyn.cate@gmail.com>
2019-05-07 20:29:49 -0400

    adds profile struct encodability and decodability, and more model controller methods pertaining to the new database

--- Unblush/AwkDate/Model Controllers/User2Controller.swift
+++ Unblush/AwkDate/Model Controllers/User2Controller.swift
@@ -26,7 +26,41 @@ class User2Controller {
                 completion(error)
                 return
             }
-        }
-    }
+
+            if let userLocal = user {
+                self.putProfileToServer(userID: userLocal.user.uid, firstName: "Bob", lastName: "Blue", email: email, dob: dateFormatter.date(from: "06 15, 1999")!, gender: "Male", zipcode: 23456, condition: ["Herpes"], mainPhoto: self.currentPhoto!, lookingFor: "Same", biography: "Nothing", completion: completion)
+            }
             
+        }
+        
+        
+    }
+    
+    func putProfileToServer(userID: String, firstName: String, lastName: String, email: String, dob: Date, gender: String, zipcode: Int, condition: [String], mainPhoto: Data, lookingFor: String, biography:String,  completion: @escaping (Error?) -> Void = {_ in }) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM dd, yyyy"
+        
+        let exampleProfile = Profile(firstName: "Joe", lastName: "Blue", email: "test14@test.com", dob: dateFormatter.date(from: "05 22, 1997")!, gender: "Male", zipcode: 23456, condition: ["Herpes"], mainPhoto: self.currentPhoto!, likedMatches: [[:]], lookingFor: "Same", biography: "Nothing really", matches: [[:]])
+        
+        let profile = Profile(firstName: firstName, lastName: lastName, email: email, dob: dob, gender: gender, zipcode: zipcode, condition: condition, mainPhoto: mainPhoto, likedMatches: [exampleProfile.toAnyObject() as NSDictionary], lookingFor: lookingFor, biography: biography, matches: [exampleProfile.toAnyObject() as NSDictionary])
+        
+        //.. /profiles/uid
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("profiles").document(userID)
+        
+        if ref != nil {
+            ref?.setData(profile.toAnyObject())
+        }
+        
+        
+        //addDocument(data: profile.toAnyObject())
+    }
+    
+    func uploadPhoto(imageContainer: Data) {
+        self.currentPhoto = imageContainer
+    }
+    
+    
 }
